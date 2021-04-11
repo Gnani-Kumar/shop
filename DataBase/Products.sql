5fd9e5b005f24e25207e0aba638ad8223c857454
Gnani <Me@DESKTOP-NLFKS97>
2021-04-11 23:38:05 +0530

    Added the database

---
+++ DataBase/Products.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Products (
+ProductId int primary key identity,
+    Name varchar(255) NOT NULL,
+    Description varchar(255) NOT NULL,
+    Price DECIMAL(20,3),
+    ProdAddedDate DATE,
+	Availability  varchar(10),
+	Brand  varchar(255)
+
+);
---
+++ DataBase/ProductsSeeds.sql
@@ -0,0 +1,19 @@
+﻿
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Shoes','For Mens','787',getdate(),'Yes','Puma');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Shoes-sneakers','Unisex','987',getdate(),'Yes','Puma');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('T-Shirt','For Mens','787',getdate(),'Yes','Puma');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Slippers','For Mens','787',getdate(),'Yes','Puma');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Pants','For Mens','787',getdate(),'Yes','Puma');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Hoodies','For Mens','787',getdate(),'Yes','Puma');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Shoes','For Mens','787',getdate(),'Yes','Adidas');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Hoodies','For Mens','787',getdate(),'Yes','Adidas');
+insert into Products (Name,Description,Price,ProdAddedDate,Availability,Brand) values
+('Pants','For Mens','787',getdate(),'Yes','Adidas');
